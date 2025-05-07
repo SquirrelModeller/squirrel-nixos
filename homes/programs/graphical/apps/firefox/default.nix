f648596c5f10795107ada82b737099cced2cd045
Squirrel Modeller <squirrelmodeller@gmail.com>
2025-05-07 13:52:38 +0200

    refactor: firefox profiling
    Privacy settings are now applied correctly.

--- homes/programs/graphical/apps/firefox/default.nix
+++ homes/programs/graphical/apps/firefox/default.nix
@@ -1,10 +1,10 @@
-{
-  config,
-  lib,
-  pkgs,
-  osConfig,
-  ...
-}: let
+{ config
+, lib
+, pkgs
+, osConfig
+, ...
+}:
+let
   lock-false = {
     Value = false;
     Status = "locked";
@@ -17,31 +17,66 @@
   inherit (osConfig) modules;
 
   env = modules.usrEnv;
-in {
+
+  privacy = {
+    "toolkit.telemetry.reportingpolicy.firstRun" = false;
+    "toolkit.telemetry.newProfilePing.enabled" = false;
+    "toolkit.telemetry.enabled" = false;
+    "toolkit.telemetry.bhrPing.enabled" = false;
+    "toolkit.telemetry.archive.enabled" = false;
+    "toolkit.telemetry.updatePing.enabled" = false;
+    "toolkit.telemetry.shutdownPingSender.enabled" = false;
+    "toolkit.telemetry.firstShutdownPing.enabled" = false;
+    "datareporting.usage.uploadEnabled" = false;
+  };
+in
+{
   config = mkIf env.programs.apps.firefox.enable {
-    home.packages = with pkgs; [
-      firefox
-    ];
     programs.firefox = {
+      enable = true;
+      profiles = {
+        default = {
+          id = 0;
+          name = "default";
+          isDefault = false;
+          search = {
+            force = true;
+            default = "ddg";
+            order = [ "ddg" "google" ];
+          };
+          settings = privacy;
+        };
+
+        squirrel = {
+          id = 1;
+          name = "squirrel";
+          isDefault = true;
+          search = {
+            force = true;
+            default = "ddg";
+            order = [ "ddg" "google" ];
+          };
+          settings = privacy // {
+            "browser.startup.page" = 3;
+            "browser.toolbars.bookmarks.visibility" = "never";
+          };
+        };
+      };
+
       policies = {
         DisableTelemetry = true;
         DisableFirefoxStudies = true;
+        DisablePocket = true;
+        DontCheckDefaultBrowser = true;
         EnableTrackingProtection = {
           Value = true;
           Locked = true;
           Cryptomining = true;
           Fingerprinting = true;
         };
-        DisablePocket = true;
-        DontCheckDefaultBrowser = true;
         Preferences = {
-          # It seems these policies are not being applied
-          # Maybe they only apply on a fresh browser?
-          "toolkit.telemetry.reportingpolicy.firstRun" = false;
-          "toolkit.telemetry.newProfilePing.enabled" = false;
-          "toolkit.telemetry.enabled" = false;
-          "toolkit.telemetry.bhrPing.enabled" = false;
-          "toolkit.telemetry.archive.enabled" = false;
+          "browser.newtabpage.activity-stream.feeds.telemetry" = lock-false;
+          "browser.newtabpage.activity-stream.telemetry" = lock-false;
         };
       };
     };
