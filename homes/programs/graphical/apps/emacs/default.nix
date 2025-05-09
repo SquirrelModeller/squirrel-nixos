8a81cfa6b7c3cc7904dfe452f8b670659f6b0444
Squirrel Modeller <squirrelmodeller@gmail.com>
2025-05-09 15:02:00 +0200

    fix: faster loading
    Applied a few settings in the early init to get faster loading.

--- homes/programs/graphical/apps/emacs/default.nix
+++ homes/programs/graphical/apps/emacs/default.nix
@@ -79,6 +79,24 @@ in
         (setq custom-file (expand-file-name "custom.el" user-emacs-directory))
         (when (file-exists-p custom-file)
           (load custom-file nil t))
+
+        (setq package-enable-at-startup nil)
+
+        (push '(menu-bar-lines . 0) default-frame-alist)
+        (push '(tool-bar-lines . 0) default-frame-alist)
+        (push '(vertical-scroll-bars) default-frame-alist)
+
+        (setq frame-inhibit-implied-resize t)
+
+        (menu-bar-mode -1)
+        (tool-bar-mode -1)
+        (scroll-bar-mode -1)
+        (setq inhibit-splash-screen t)
+        (setq use-dialog-box nil)
+
+        (setq gc-cons-threshold most-positive-fixnum)
+        (setq gc-cons-percentage 0.6)
+
         (load-file (expand-file-name "config.el" user-emacs-directory))
       '';
     };
