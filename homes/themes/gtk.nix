888047fbbdf33a8caea3ceed02456fad8a6e3e13
Squirrel Modeller <squirrelmodeller@gmail.com>
2025-05-16 11:32:47 +0200

    refactor: disable cat theme
    I will modify the gtk theme later to be dependent on the "global" theme
    and match the colors.

--- homes/themes/gtk.nix
+++ homes/themes/gtk.nix
@@ -10,18 +10,18 @@ let
   env = modules.usrEnv;
 in
 {
-  imports = [ inputs.catppuccin.homeModules.catppuccin ];
+  #imports = [ inputs.catppuccin.homeModules.catppuccin ];
   config = mkIf env.style.gtk.enable {
-    gtk = {
-      enable = true;
-      catppuccin = {
-        enable = true;
-        flavor = "mocha";
-        accent = "peach";
-        size = "standard";
-        tweaks = [ "normal" ];
-      };
-    };
+    # gtk = {
+    #   enable = true;
+    #   catppuccin = {
+    #     enable = true;
+    #     flavor = "mocha";
+    #     accent = "peach";
+    #     size = "standard";
+    #     tweaks = [ "normal" ];
+    #   };
+    # };
 
     dconf.settings = {
       "org/gnome/desktop/wm/preferences" = {
