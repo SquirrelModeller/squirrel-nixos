4bcacf3506cf9c8a08afd41309ab90c2468f4653
Squirrel Modeller <squirrelmodeller@gmail.com>
2025-08-22 12:57:37 +0200

    chore: add u2f key

--- modules/terminal/security.nix
+++ modules/terminal/security.nix
@@ -1,8 +1,13 @@
 { pkgs, lib, ... }:
 let
-  u2fLines = [
-    "squirrel:vtjRC1Qun6lE/+su1eOT0DpMEP33/kt34tS+vFMrQ5ytPSoCTisV6xbKUrj2KxV4wVm9ygspmuI3n3TYwSLI5g==,NKloYTARrswjUQg2h0oqdObwyqCHOGXRmqTsRrhKR/5vZMW7Dsf+4PTiZ3qil68hXDlwXvD9WUCZrPaUExHHFg==,es256,+presence"
-  ];
+  u2fCreds = {
+    squirrel = [
+      "vtjRC1Qun6lE/+su1eOT0DpMEP33/kt34tS+vFMrQ5ytPSoCTisV6xbKUrj2KxV4wVm9ygspmuI3n3TYwSLI5g==,NKloYTARrswjUQg2h0oqdObwyqCHOGXRmqTsRrhKR/5vZMW7Dsf+4PTiZ3qil68hXDlwXvD9WUCZrPaUExHHFg==,es256,+presence"
+      "91wYxEuarNfRfzxL0wz73aaIniVmpJ5gP6LToWvQiKQ7+rElk5VcuK3W4YufIhzTx83kEYoJQJISEqsiEBXxiA==,wDI1zibPz39ilFcNKqOdYtn76gvj/suZF8Y+dxu9wUMCa+kF8WWWPdsuN5gQ7vPRQ0EuUoKB6/9k2XLLrLTX6A==,es256,+presence"
+    ];
+  };
+  mkLine = user: creds: "${user}:" + lib.concatStringsSep ":" creds;
+  u2fLines = lib.attrValues (lib.mapAttrs mkLine u2fCreds);
 in
 {
   services.pcscd.enable = true;
