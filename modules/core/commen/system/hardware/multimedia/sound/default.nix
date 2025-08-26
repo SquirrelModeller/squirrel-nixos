7271e8f2478caad269f98422ad8d195758a96bd2
Squirrel Modeller <squirrelmodeller@protonmail.com>
2025-08-26 21:00:44 +0200

    fix: remove hardcoded sound target

--- modules/core/commen/system/hardware/multimedia/sound/default.nix
+++ modules/core/commen/system/hardware/multimedia/sound/default.nix
@@ -1,9 +1,4 @@
 {
-  config,
-  lib,
-  pkgs,
-  ...
-}: {
   security.rtkit.enable = true;
 
   services.pipewire = {
@@ -11,23 +6,8 @@
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
+    jack.enable = true;
   };
 
   services.pipewire.wireplumber.enable = true;
-  # I found that wpctl worked fine to set the default sound
-  # I have not configured that yet though in these files
-  # This is left here until i figure out how to properly configure the sound
-  environment.etc."wireplumber/policy.lua.d/90-default-audio.lua".text = ''
-    table.insert(alsa_monitor.rules, {
-      matches = {
-        {
-          { "node.name", "equals", "alsa_output.usb-iFi__by_AMR__iFi__by_AMR__HD_USB_Audio_0003-00.analog-stereo" },
-        },
-      },
-      apply_properties = {
-        ["node.disabled"] = false,
-        ["priority.session"] = 10000,
-      }
-    })
-  '';
 }
