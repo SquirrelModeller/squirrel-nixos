4e09fe08adcdc5b9252575c37f663f056fdcf701
Squirrel Modeller <squirrelmodeller@protonmail.com>
2026-02-02 11:07:03 +0100

    feat: vscodium

---
+++ modules/graphical/apps/vscodium.nix
@@ -0,0 +1,42 @@
+{ pkgs, ... }:
+
+{
+  programs.direnv = {
+    enable = true;
+    enableBashIntegration = true;
+    enableZshIntegration = true;
+    nix-direnv.enable = true;
+  };
+
+  programs.vscode = {
+    enable = true;
+    package = pkgs.vscodium;
+
+    extensions = with pkgs.vscode-extensions; [
+      jnoortheen.nix-ide
+
+      ms-python.python
+
+      ms-toolsai.jupyter
+      ms-toolsai.jupyter-keymap
+      ms-toolsai.jupyter-renderers
+
+      mkhl.direnv
+
+      eamodio.gitlens
+
+      yzhang.markdown-all-in-one
+
+      usernamehw.errorlens
+
+    ];
+  };
+
+  environment.systemPackages = with pkgs; [
+    nil
+    nixpkgs-fmt
+  ];
+
+  programs.bash.enable = true;
+  programs.zsh.enable = true;
+}
